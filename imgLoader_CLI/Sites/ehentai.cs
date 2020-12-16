30350e6fb3b976864f64499a21e6d11acfdcff7e
Waterfront <junn661@gmail.com>
2020-12-16 20:59:17 +0900

    Add ehentai

---
+++ imgLoader_CLI/Sites/ehentai.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imgLoader_CLI.Sites
+{
+    class ehentai : ISite
+    {
+        internal static string[] Supplement = {
+            "reader"
+        };
+
+        internal static string Host = "hitomi.la";
+
+        private static readonly string[] FILTER = { " - Read Online", " - hentai doujinshi", "  Hitomi.la", " | Hitomi.la" };
+        private static readonly string[] REPLACE = { "", "", "", "" };
+
+        private readonly string _source;
+        private readonly string _number;
+        private string _artist;
+
+        public ehentai(string mNumber)
+        {
+
+        }
+
+        public string GetArtist()
+        {
+            return "";
+        }
+
+        public Dictionary<string, string> GetImgUrls()
+        {
+
+        }
+
+        public string GetTitle();
+
+        public string[] ReturnInfo();
+
+        public bool IsValidated();
+    }
+}
