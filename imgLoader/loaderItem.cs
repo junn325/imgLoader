210cd7775a2158190a771838c54928d541f23ca3
Waterfront <junn661@gmail.com>
2021-01-26 06:13:56 +0900

    1.26

---
+++ imgLoader/loaderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace imgLoader
+{
+    public partial class loaderItem : UserControl
+    {
+        public loaderItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
