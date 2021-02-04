22bfd2ba7ba67cb8d840c393d387115b662dd9a6
unassigned <18192378+junn661@users.noreply.github.com>
2021-02-05 04:52:00 +0900

    TagItem 추가

---
+++ imgLoader_WPF/Tag/TagItem.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace imgLoader_WPF.Tag
+{
+    public partial class TagItem
+    {
+        public string TagName
+        {
+            get => Tag.Content.ToString();
+            set => Tag.Content = value;
+        }
+
+        public SColor Sex
+        {
+            get => this.Background == (Brush)new BrushConverter().ConvertFrom("#E86441") ? SColor.Female : SColor.Male;
+            set => this.Background = (value == SColor.Female ? (Brush)new BrushConverter().ConvertFrom("#E86441") : (Brush)new BrushConverter().ConvertFrom("#00A2FF"));
+        }
+
+        public enum SColor
+        {
+            Female,
+            Male
+        }
+        public TagItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
