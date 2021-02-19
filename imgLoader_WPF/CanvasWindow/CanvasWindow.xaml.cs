5ade9e8e8cc0889f6063e9cf35a80e7a7e09ce0b
unassigned <18192378+junn661@users.noreply.github.com>
2021-02-19 23:17:42 +0900

    2.19

--- imgLoader_WPF/CanvasWindow/CanvasWindow.xaml.cs
+++ imgLoader_WPF/CanvasWindow/CanvasWindow.xaml.cs
@@ -71,59 +71,68 @@ namespace imgLoader_WPF.CanvasWindow
             }
         }
 
-        private const byte Delta = 50;
-        private Rect relRect;
+        private const byte Scale = 15; //percent
+        private Rect _relRect;
         private void Window_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
-            if (e.MiddleButton == MouseButtonState.Pressed)
+            if (e.MiddleButton != MouseButtonState.Pressed) return;
+
+            var temp = Mouse.GetPosition(Container);
+            var thisPos = Container.TransformToAncestor(this).Transform(new Point(0, 0));
+
+            if (_relRect.Width == 0 || _relRect.Height == 0) _relRect = new Rect(thisPos.X, thisPos.Y, Container.ActualWidth, Container.ActualHeight);
+
+            var xRightPct = temp.X / Container.ActualWidth;
+            var xLeftPct = (Container.ActualWidth - temp.X) / Container.ActualWidth;
+            var yTopPct = temp.Y / Container.ActualHeight;
+            var yBottPct = (Container.ActualHeight - temp.Y) / Container.ActualHeight;
+
+            //var rect = new Rectangle
+            //{
+            //    Width = relRect.Width,
+            //    Height = relRect.Height,
+            //    Fill = Brushes.Green,
+            //    Stroke = Brushes.Red,
+            //    StrokeThickness = 2
+            //};
+
+            //Canvas1.Children.Add(rect);
+
+            if (e.Delta > 0)
             {
-                var temp = Mouse.GetPosition(Container);
-                var thisPos = Container.TransformToAncestor(this).Transform(new Point(0, 0));
+                _relRect.X -= Delta * xRightPct;
+                _relRect.Y -= Delta * yTopPct;
+                _relRect.Width += Delta * xLeftPct;
+                _relRect.Height += Delta * yBottPct;
 
-                if (relRect.Width == 0 || relRect.Height == 0) relRect = new Rect(thisPos.X, thisPos.Y, Container.ActualWidth, Container.ActualHeight);
+                Container.Stretch = Stretch.Uniform;
+                Container.Arrange(_relRect);
+            }
+            else
+            {
+                _relRect.X += Delta * xRightPct;
+                _relRect.Y += Delta * yTopPct;
+                _relRect.Width -= Delta * xLeftPct;
+                _relRect.Height -= Delta * yBottPct;
 
-                var xRightPct = temp.X / Container.ActualWidth;
-                var xLeftPct = (Container.ActualWidth - temp.X) / Container.ActualWidth;
-                var yTopPct = temp.Y / Container.ActualHeight;
-                var yBottPct = (Container.ActualHeight - temp.Y) / Container.ActualHeight;
-
-                //var rect = new Rectangle
-                //{
-                //    Width = relRect.Width,
-                //    Height = relRect.Height,
-                //    Fill = Brushes.Green,
-                //    Stroke = Brushes.Red,
-                //    StrokeThickness = 2
-                //};
-
-                //Canvas1.Children.Add(rect);
-
-                if (e.Delta > 0)
-                {
-                    relRect.X -= Delta * xRightPct;
-                    relRect.Y -= Delta * yTopPct;
-                    relRect.Width += Delta * xLeftPct;
-                    relRect.Height += Delta * yBottPct;
-
-                    Container.Stretch = Stretch.Uniform;
-                    Container.Arrange(relRect);
-                }
-                else
-                {
-                    relRect.X += Delta * xRightPct;
-                    relRect.Y += Delta * yTopPct;
-                    relRect.Width -= Delta * xLeftPct;
-                    relRect.Height -= Delta * yBottPct;
-
-                    Container.RenderTransform = new ScaleTransform(-Delta * xLeftPct, -Delta * yBottPct, Container.ActualWidth / 2, Container.ActualHeight / 2);
-                    //Canvas.SetLeft(Container, relRect.X);
-                }
+                Container.RenderTransform = new ScaleTransform(1, 1, 50, 50);
+                //Canvas.SetLeft(Container, relRect.X);
             }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             _imgHandler = Image;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Container.Width = Container.ActualWidth + 50;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _relRect = new Rect(0, 0, 0, 0);
         }
     }
 }
