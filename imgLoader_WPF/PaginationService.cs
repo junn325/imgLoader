2dab39a465435f5b7aab293a0e305e39a5b5d75a
unassigned <18192378+junn661@users.noreply.github.com>
2021-02-27 09:37:36 +0900

    2.27

--- imgLoader_WPF/PaginationService.cs
+++ imgLoader_WPF/PaginationService.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using imgLoader_WPF.LoaderListCtrl;
+
+using System;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-
-using imgLoader_WPF.LoaderListCtrl;
 
 namespace imgLoader_WPF
 {
@@ -37,22 +33,24 @@ namespace imgLoader_WPF
             if (_service.ThreadState != System.Threading.ThreadState.Stopped) return;
 
             page: _service = new Thread(() =>
-             {
-                 while (_actualIndex.Count == 0)
-                 {
-                     Task.Delay(500).Wait();
-                 }
+            {
+                _scroll.Dispatcher.Invoke(() => _scroll.ScrollToTop());
 
-                 int oriCnt = _showItems.Count;
-                 for (int i = 0; i < Math.Ceiling(_scroll.ActualHeight / LoaderItem.MHeight); i++)
-                 {
-                     var i1 = i;
-                     if (oriCnt + i1 + 1 > _actualIndex.Count) return;
+                while (_actualIndex.Count == 0)
+                {
+                    Task.Delay(500).Wait();
+                }
 
-                     var temp = _actualIndex[oriCnt + i1];
-                     _sender.Dispatcher.Invoke(() => _showItems.Add(temp));
-                 }
-             });
+                int oriCnt = _showItems.Count;
+                for (int i = 0; i < Math.Ceiling(_scroll.ActualHeight / LoaderItem.MHeight); i++)
+                {
+                    var i1 = i;
+                    if (oriCnt + i1 + 1 > _actualIndex.Count) return;
+
+                    var temp = _actualIndex[oriCnt + i1];
+                    _sender.Dispatcher.Invoke(() => _showItems.Add(temp));
+                }
+            });
             _service.Name = "PgSvc";
             _service.Start();
         }
