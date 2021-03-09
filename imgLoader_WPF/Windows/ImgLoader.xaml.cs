5ceb0885c148d647eb8a392f946917b61ba632e4
unassigned <18192378+junn661@users.noreply.github.com>
2021-03-09 22:23:16 +0900

    정렬 전 백업

--- imgLoader_WPF/Windows/ImgLoader.xaml.cs
+++ imgLoader_WPF/Windows/ImgLoader.xaml.cs
@@ -85,10 +85,6 @@ namespace imgLoader_WPF.Windows
 
         private void ImgLoader_WPF_Loaded(object sender, RoutedEventArgs e)
         {
-            //var temp = new ChangeIP();
-
-            //var a = temp.ChangeMACAddress("2C-F0-5D-0A-B0-25");
-
             Menu.Focus(); //메뉴 미리 로드
             _winSetting = new Settings(this, Scroll, _index);
 
