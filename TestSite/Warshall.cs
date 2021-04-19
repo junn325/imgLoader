0eca066e6250611ef2a7ca408b6d398f84816746
unassigned <18192378+junn661@users.noreply.github.com>
2021-04-19 22:37:38 +0900

    4.19

---
+++ TestSite/Warshall.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace TestSite
+{
+    public static class Warshall
+    {
+        public static bool[,] DoWarshall(bool[,] m)
+        {
+            var n = m.GetLength(0);
+            var w = m;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        w[i, j] = w[i, j] || (w[i, k] && w[k, j]);
+
+                    }
+                }
+            }
+
+            return w;
+        }
+
+        public static void Print2DArr(bool[,] arr)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sb.Append(arr[i, j] ? "1" : "0").Append(' ');
+                }
+
+                sb.Append('\n');
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
