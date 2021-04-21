ae25f6d3d5c50ea579aeaefbec02d5f8e7463a90
unassigned <18192378+junn661@users.noreply.github.com>
2021-04-22 06:33:31 +0900

    4.22

--- TestSite/Program.cs
+++ TestSite/Program.cs
@@ -11,34 +11,6 @@ namespace TestSite
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("2진수 입력시 0b 접두사 필요; 자리수 구분자 _ 자동으로 무시");
-            //while (true)
-            //{
-            //    var temp = Console.ReadLine().Replace("_", "");
-
-            //    try
-            //    {
-            //        temp = Convert.ToString(Convert.ToSByte(temp), 2);
-            //    }
-            //    catch
-            //    {
-            //        Console.WriteLine("숫자가 아니거나 너무 큽니다. 다시 시도하세요.");
-            //        continue;
-            //    }
-
-            //    TwosComplement.Calc(Convert.ToSByte(temp.Length >= 8 ? temp[8..] : temp.PadLeft(8, '0'), 2));
-            //}
-
-            var temp = Warshall.DoWarshall(new bool[,] {
-                { true, false, false, true, false },
-                { false, true, false, false, false },
-                { false, false, false, true, true },
-                { true, false, false, false, false },
-                { false, true, false, false, true }
-
-            });
-
-            Warshall.Print2DArr(temp);
         }
 
     }
