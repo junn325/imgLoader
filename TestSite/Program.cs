5965f490666a3a0dfbbea7c9cc917dae9c459a53
unassigned <18192378+junn661@users.noreply.github.com>
2021-04-21 21:54:12 +0900

    4.21

--- TestSite/Program.cs
+++ TestSite/Program.cs
@@ -30,11 +30,11 @@ namespace TestSite
             //}
 
             var temp = Warshall.DoWarshall(new bool[,] {
-                { true, false, false, true, false },
-                { false, true, false, false, false },
-                { false, false, false, true, true },
-                { true, false, false, false, false },
-                { false, true, false, false, true }
+                { false, false, true, true, false },
+                { true, false, true, false, false },
+                { false, false, false, false, false },
+                { false, false, false, false, true },
+                { false, false, false, false, false }
 
             });
 
