abfdcbb1b9d4211415c9082c5f95308a80d1df80
unassigned <18192378+junn661@users.noreply.github.com>
2021-04-30 21:39:13 +0900

    Categorizer 생성

---
+++ imgLoader_WPF/Services/Categorizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using imgLoader_WPF.Windows;
+
+namespace imgLoader_WPF.Services
+{
+    internal class Categorizer
+    {
+        internal List<List<string>> CategoryList = new();
+
+        public Categorizer(ImgLoader sender)
+        {
+
+        }
+    }
+}
